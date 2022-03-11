df0c2d7e22a567d963dd3e0f4bb87a8fd98ae046
zachwheels <97123692+zachwheels@users.noreply.github.com>
2022-03-10 18:15:50 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,9 @@
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
-    host: 'classmysql.engr.oregonstate.edu',
-    user: 'cs340_wheelihz',
-    password: '1014',
-    database: 'cs340_wheelihz',
+    host: 'uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'ae6l65zdt4mk9tmg',
+    password: 'eo20dq6pd2zhng24',
+    database: 'wb28n4k083fa87aq',
     connectionLimit: 5
 });
 
